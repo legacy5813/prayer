0b33d4d42adbf059e3c0ee153740a6fc242b4408
xZifhuba <hafiirahman200@gmail.com>
2022-05-10 22:04:10 +0100

    respone data useState update

--- src/Fahim.js
+++ src/Fahim.js
@@ -1,16 +1,32 @@
 import React from "react";
+import { useState } from "react";
 import Button from 'react-bootstrap/Button'
 import axios from "axios";
 
 const Fahim = () => {
+    //empty usestates for setting each 5 prayer times to display
+    const [fajr, setFajr] = useState('');
+    const [sunrise, setSunrise] = useState('');
+    const [dhuhr, setDhuhr] = useState('');
+    const [asr, setAsr] = useState('');
+    const [maghrib, setMaghrib] = useState('');
+    const [isha, setIsha ] = useState('');
+
+    const PrayerTimes = []; 
     axios.get('http://api.aladhan.com/v1/timingsByCity/:date_or_timestamp', {
         params: { //parameters narrowing down request data for API
             city: 'Mansfield',
             country: 'GB'
         }
     })
-    .then(function (response) {
-        console.log(response); //outputting reponse
+    .then(function (response) {//response.data.data.timings accesses the times contains two data points
+        //Updating usestates for each prayer times from the API response
+        setFajr(response.data.data.timings.Fajr);
+        setSunrise(response.data.data.timings.Sunrise);
+        setDhuhr(response.data.data.timings.Dhuhr);
+        setAsr(response.data.data.timings.Asr);
+        setMaghrib(response.data.data.timings.Maghrib);
+        setIsha(response.data.data.timings.Isha);
     })
     .catch(function (error) { //catching any errors
         console.log(error);
