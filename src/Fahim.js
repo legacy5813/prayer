86f78c930fbb54b639e27e8fe28e03ff39f695bb
xZifhuba <hafiirahman200@gmail.com>
2022-05-10 20:50:53 +0100

    Prayer API setup

--- src/Fahim.js
+++ src/Fahim.js
@@ -1,12 +1,25 @@
 import React from "react";
 import Button from 'react-bootstrap/Button'
+import axios from "axios";
 
 const Fahim = () => {
+    axios.get('http://api.aladhan.com/v1/timingsByCity/:date_or_timestamp', {
+        params: { //parameters narrowing down request data for API
+            city: 'Mansfield',
+            country: 'GB'
+        }
+    })
+    .then(function (response) {
+        console.log(response); //outputting reponse
+    })
+    .catch(function (error) { //catching any errors
+        console.log(error);
+    });
 
     return ( 
         <div>
-            <h1>Prayer!!!</h1>
-            <Button variant="info">Click me Fahim</Button>
+            <h1 className="title">Prayer</h1>
+            <Button variant="success">Click me Fahim</Button>
 
         </div>
      );
